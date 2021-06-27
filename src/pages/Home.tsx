602453245737e80cf7858fb9a4306bb1f1d6e4e1
CrazyGladiator <virtuoso751@gmail.com>
2021-06-26 21:52:01 -0300

    Aula 03
    Feito as Rooms e suas funcionalidades de criação das mesmas.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { useHistory } from 'react-router-dom';
+import { FormEvent, useState } from 'react';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
@@ -8,6 +9,8 @@ import { Button } from '../components/Button';
 import { useAuth } from '../hooks/useAuth';
 
 import '../styles/auth.scss';
+import { database } from '../services/firebase';
+
 
 
 
@@ -17,7 +20,8 @@ export function Home(){
 
   //só pode ser usada dentro do Export, fora não funcionará.
   const history = useHistory();
-  const { user, signInWithGoogle } =  useAuth()
+  const { user, signInWithGoogle } =  useAuth();
+  const[roomCode, setRoomCode] = useState('');
 
   async function handleCreateRoom(){
 
@@ -28,6 +32,29 @@ export function Home(){
     }
 
       history.push('/rooms/new');
+
+  }
+
+  async function handleJoinRoom(event: FormEvent){
+
+    event.preventDefault();
+
+    if(roomCode.trim()===''){
+
+      return;
+
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if(!roomRef.exists()){
+
+      alert('Room does not exists.');
+      return;
+      
+    }
+
+    history.push(`rooms/${roomCode}`);
 
   }
 
@@ -60,11 +87,13 @@ export function Home(){
 
           <div className="separator">Ou entre em uma sala</div>
 
-          <form >
+          <form onSubmit={handleJoinRoom}>
 
             <input
                type="text" 
                placeholder="Digite o código da sala"
+               onChange= {event => setRoomCode(event.target.value)}
+               value={roomCode}
             />
             
             <Button type="submit">
